9baea10919483affe80f3273f6e8047ef754b615
alejandro.caceres.dominguez <alejandro.caceres.dominguez@auleseebe.upc.edu>
2024-09-26 11:10:59 +0200

    problema 2

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,44 @@
 #Codigo para problema 2
+iris
+mis_dades <- iris
+
+y <- mis_dades$Sepal.Length
+y
+
+x <- mis_dades$Petal.Length
+x
+
+plot(x,y)
+
+xbar <- mean(x)
+xbar
+
+ybar <- mean(y)
+ybar
+
+m <- sum((x-xbar)*(y-ybar))/sum((x-xbar)^2)
+m
+b <- ybar -m*xbar
+b
+
+m*1.5+b
+
+mod <- lm(y~x)
+
+data.frame(x=x)
+
+ypredicted <- predict(mod, data.frame(x=x))
+
+plot(x,ypredicted, type="l")
+points(x, y)
+
+
+plot(x,y, pch=16, col="red")
+lines(x, ypredicted)
+
+Rsq <- sum((ypredicted-ybar)^2)/sum((y-ybar)^2)
+Rsq
+
+summary(mod)
+sqrt(Rsq)
+cor.test(x,y)
