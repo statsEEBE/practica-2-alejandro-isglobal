cec154ba5903a37f3d5f13991b4faef30e7dad1e
alejandro.caceres.dominguez <alejandro.caceres.dominguez@auleseebe.upc.edu>
2024-10-03 11:06:23 +0200

    ...

--- Problema2.R
+++ Problema2.R
@@ -1,44 +1,45 @@
-#Codigo para problema 2
-iris
-mis_dades <- iris
+# Problema 2
 
-y <- mis_dades$Sepal.Length
-y
+mis_dades <- iris
+mis_dades
 
 x <- mis_dades$Petal.Length
-x
+y <- mis_dades$Sepal.Length
 
-plot(x,y)
+plot(x, y)
 
-xbar <- mean(x)
-xbar
+x_bar <- mean(x)
+y_bar <- mean(y)
 
-ybar <- mean(y)
-ybar
-
-m <- sum((x-xbar)*(y-ybar))/sum((x-xbar)^2)
+m <- sum((x-x_bar)*(y-y_bar))/sum((x-x_bar)^2)
 m
-b <- ybar -m*xbar
+
+b <- y_bar - m*x_bar
 b
 
 m*1.5+b
 
-mod <- lm(y~x)
+x_pred <- x
+x_pred
+y_pred <- m*x_pred+b
+y_pred
 
-data.frame(x=x)
+plot(x, y)
+lines(x_pred, y_pred)
 
-ypredicted <- predict(mod, data.frame(x=x))
-
-plot(x,ypredicted, type="l")
-points(x, y)
-
-
-plot(x,y, pch=16, col="red")
-lines(x, ypredicted)
-
-Rsq <- sum((ypredicted-ybar)^2)/sum((y-ybar)^2)
+Rsq <- sum((y_pred-y_bar)^2)/sum((y-y_bar)^2)
 Rsq
 
+cor <- sqrt(Rsq)
+cor
+
+mod <- lm(y~x)
+mod
+
 summary(mod)
-sqrt(Rsq)
-cor.test(x,y)
+
+cor.test(x, y)
+
+y_pred2 <- predict(mod, data.frame(x=1.5))
+
+y_pred2
